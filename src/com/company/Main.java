cee4a38f68112e1dd102a6eeccf3acd65583c122
Belinda chanda <btatend1@stu.vistula.edu.pl>
2022-05-28 22:58:59 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {RectangleArea {
+        double a , b, area ;
+
+        void getDate() throws IOException{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Enter the length of side a : ");
+            a = Double.parseDouble(br.readLine());
+            System.out.println("Enter the length of side b : ");
+            b = Double.parseDouble(br.readLine());
+        }
+        void computeField () { area = a * b ;}
+
+        void fieldDisplay(){
+            System.out.print("The area of the rectangle with the side a =   ");
+            System.out.printf("%2.2f", a);
+            System.out.print(" and side b = ");
+            System.out.printf("%2.2f",b);
+            System.out.print("is = ");
+            System.out.printf("%2.2f \n ", area );
+        }
+	// write your code here
+    }
+}
